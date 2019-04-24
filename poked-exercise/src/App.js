1cf4dce36d89dbc9929b9f090d17e22ac8908c6d
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-04-24 15:31:49 +0100

    Add Pokegame

---
+++ poked-exercise/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Pokegame from './PokeGame';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Pokegame />
+    </div>
+  );
+}
+
+export default App;
